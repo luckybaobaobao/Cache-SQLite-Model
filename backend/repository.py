4171f229a3764d0a1bdd6b114d26ef107a634c2f
“Lina” <lesleylucky1101@gmail.com>
2023-01-24 15:25:36 +0100

    Add update by id function

--- backend/repository.py
+++ backend/repository.py
@@ -4,3 +4,9 @@ from db import session
 def insert(item: object):
     session.add(item)
     session.commit()
+
+def update_by_external_id(item: object, external_id: str, values: dict):
+    _item = session.query(item).filter_by(external_id=external_id)
+    _item.update(values)
+    session.commit()
+    return _item.first()
