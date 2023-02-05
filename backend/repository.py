036a1a67d4ad02b3936bfcbc9e1d56dee374bce0
“Lina” <lesleylucky1101@gmail.com>
2023-02-05 20:27:13 +0100

    Add free search function

--- backend/repository.py
+++ backend/repository.py
@@ -16,3 +16,7 @@ def search_by_name(item: object, value: str):
 
 def search_by_external_id(item: object, value: str):
     return session.query(item).filter_by(external_id=value).first()
+
+
+def free_search(item: object, text: str):
+    return session.query(item).filter(item.value.like(text)).all()
