ed449c6dd178c65c6e0ce93de2c614de9d037a42
“Lina” <lesleylucky1101@gmail.com>
2023-02-13 19:12:56 +0100

    Add post endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
+import controller
 from cache import Cache
 from db import create_tables
+
 
 cache = Cache()
 app = Flask(__name__)
 
+
+@app.route("/api/jokes/", methods=["POST"])
+def insert_joke():
+    joke_details = request.get_json()
+    category = joke_details.get("categories", [])
+    icon_url = joke_details.get("icon_url", None)
+    value = joke_details.get("value", None)
+    if not value:
+        abort(422, "The value of joke is necessary")
+    result = controller.insert_joke(icon_url, value, category, cache)
+    return jsonify(result)
+
+
 if __name__ == "__main__":
     create_tables()
     app.run(host='0.0.0.0', port=8000)
+
+
