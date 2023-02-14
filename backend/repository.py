d522fa00aba337ab49f10ef4438935796c83ee8d
“Lina” <lesleylucky1101@gmail.com>
2023-02-14 21:10:01 +0100

    Add delete by joke function in repository

--- backend/repository.py
+++ backend/repository.py
@@ -5,14 +5,17 @@ def insert(item: object):
     session.add(item)
     session.commit()
 
+
 def update_by_external_id(item: object, external_id: str, values: dict):
     _item = session.query(item).filter_by(external_id=external_id)
     _item.update(values)
     session.commit()
     return _item.first()
 
+
 def search_by_name(item: object, value: str):
     return session.query(item).filter_by(name=value).first()
+
 
 def search_by_external_id(item: object, value: str):
     return session.query(item).filter_by(external_id=value).first()
@@ -20,3 +23,14 @@ def search_by_external_id(item: object, value: str):
 
 def free_search(item: object, text: str):
     return session.query(item).filter(item.value.like(text)).all()
+
+
+def delete_by_joke_id(item: object, joke_id: str):
+    session.query(item).filter_by(joke_id=joke_id).delete()
+    session.commit()
+
+
+def delete_by_id(item: object, _id: str):
+    session.query(item).filter_by(id=_id).delete()
+    session.commit()
+
