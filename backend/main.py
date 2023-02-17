9ed38be43d85ba997ae6d3bee1fe40e399ef9d2f
“Lina” <lesleylucky1101@gmail.com>
2023-02-17 21:12:43 +0100

    Add update endpoint

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,20 @@ def insert_joke():
     return jsonify(result)
 
 
+@app.route("/api/joke/<id>", methods=["PUT"])
+def update_joke(id):
+    if id not in cache.local_ids:
+        abort(404, "This joke does not exist in local database, you can not update")
+
+    joke_details = request.get_json()
+    icon_url = joke_details.get("icon_url", None)
+    value = joke_details.get("value", None)
+    category = joke_details.get("categories", None)
+
+    result = controller.update_joke(id, icon_url, value, category)
+    return jsonify(result)
+
+
 if __name__ == "__main__":
     create_tables()
     app.run(host='0.0.0.0', port=8000)
